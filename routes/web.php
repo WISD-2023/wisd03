25c1122ae253eb25611e23c4ff89f40462c88c02
劉軒宏 <3b032110@gm.student.ncut.edu.tw>
2023-12-04 13:45:18 +0800

    dd.d1

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,10 @@ Route::get('/bb/b2',function (){
     return view('bb.b2');
 });
 
+Route::get('/dd/d1',function (){
+    return view('dd.d1');
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
